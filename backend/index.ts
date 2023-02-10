ae163294b3a0502777186b826e1b61ca4c004b41
CheesyPhoenix <sindre.moe@eviny.no>
2023-02-10 13:21:11 +0100

    move to prisma

---
+++ backend/index.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const prisma = new PrismaClient();
+
+// const user = await prisma.user.create({
+// 	data: {
+// 		name: "Bob kåre4",
+// 		posts: {
+// 			create: {
+// 				title: "A post",
+// 				content: "The first one",
+// 			},
+// 		},
+// 	},
+// });
+
+// console.log(user);
+
+const users = await prisma.user.findMany({ include: { posts: true } });
+
+// const post = await prisma.post.create({
+// 	data: {
+// 		title: "Another post",
+// 		content: "The second one",
+// 		authorId: users[0].id,
+// 	},
+// });
+
+console.dir(users, { depth: null });
+
+await prisma.$disconnect();
